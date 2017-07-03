209cf03fc3565583fe2563dceadfc3cbf16c2e34
Tatyana Lebedeva <t.lebedeva@netbox.ru>
2017-07-03 17:17:31 +0300

    удаление data/result.js

--- js/views/result-view.js
+++ js/views/result-view.js
@@ -1,5 +1,4 @@
 import AbstractView from "./abstract-view.js";
-import result from "../data/result";
 import timeFormat from "../helpers/time-format";
 
 export default class ResultView extends AbstractView {
@@ -7,19 +6,23 @@ export default class ResultView extends AbstractView {
     super();
     this._correctAnswers = finalState.correctAnswers;
     this._time = timeFormat(finalState.totalTime);
+    this._result = finalState.gameStatus;
     this._stat = finalState.lowerResult;
-    this._result = result[finalState.gameStatus];
   }
 
   get template() {
     return `
       <section class="main main--result" id="result-fail">
         <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
-        <h2 class="title">${this._result.title}</h2>
-         ${this._result.stat.comparison ?
-          ` <div class="main-stat">За&nbsp;${this._time}<br>вы&nbsp;отгадали ${this._correctAnswers}&nbsp;мелодии</div>
-          <span class="main-comparison">Это&nbsp;лучше чем у&nbsp;${this._stat}%&nbsp;игроков</span>`
-          : `<div class="main-stat">${this._result.stat.text}</div>`}
+      ${(this._result === `success`) ?
+        `
+        <h2 class="title">Вы настоящий меломан!</h2>
+ <div class="main-stat">За&nbsp;${this._time}<br>вы&nbsp;отгадали ${this._correctAnswers}&nbsp;мелодии</div>
+        <span class="main-comparison">Это&nbsp;лучше чем у&nbsp;${this._stat}%&nbsp;игроков</span>
+        `
+      : `
+        <h2 class="title">Вы проиграли</h2>
+        <div class="main-stat">Ничего, вам повезет в следующий раз</div>`}
         <span role="button" tabindex="0" class="main-replay">Сыграть ещё раз</span>
       </section>
     `.trim();
