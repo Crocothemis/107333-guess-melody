cb7d6184eb86ed2e6cd5eb23cdc626ea06ce58b2
Tatyana Lebedeva <t.lebedeva@netbox.ru>
2017-07-03 20:15:22 +0300

    eslint

--- js/preload/preload-music.js
+++ js/preload/preload-music.js
@@ -1,39 +1,38 @@
 class PreloadMusic {
 
-    async loadMusic(url) {
-        if (url === ``) {
-            return Promise.resolve();
-        }
+  async loadMusic(url) {
+    if (url === ``) {
+      return Promise.resolve();
+    }
 
-        return new Promise((resolve) => {
-            var audio = new Audio();
-            audio.addEventListener(`canplaythrough`, function () {
-                resolve(true);
-            }, false);
-            audio.src = url;
+    return new Promise((resolve) => {
+      const audio = new Audio();
+      audio.addEventListener(`canplaythrough`, function () {
+        resolve(true);
+      }, false);
+      audio.src = url;
+    });
+  }
+
+  async startPreloadMusic(urls) {
+    return Promise.all(urls.map(this.loadMusic));
+  }
+
+  async init(value) {
+    const urls = [];
+    value.forEach((elem) => {
+      if (elem.type === `genre`) {
+        elem.answers.forEach((answer) => {
+          if (urls.indexOf(answer.src) === -1) {
+            urls.push(answer.src);
+          }
         });
-    }
-
-    async startPreloadMusic(urls) {
-        return Promise.all(urls.map(this.loadMusic));
-    }
-
-    async init(value) {
-        const urls = [];
-        value.forEach((elem) => {
-            if (elem.type === `genre`) {
-                elem.answers.forEach((answer) => {
-                    if (urls.indexOf(answer.src) === -1) {
-                        urls.push(answer.src);
-                    }
-                });
-            } else {
-                urls.push(elem.src);
-            }
-            
-        });
-        return this.startPreloadMusic(urls);
-    }
+      } else {
+        urls.push(elem.src);
+      }
+    });
+    return this.startPreloadMusic(urls);
+  }
 }
 
 export default new PreloadMusic();
